c99203a1b0eb7a0afe934be470ff9872a680c6b5
Kanishk Jain <kanishkbjain@gmail.com>
2020-03-06 10:31:26 -0500

    Replace call to findListDistances.c with pdist2
    Matlab's pdist2 is now 10x faster  for O(10k) points.

--- t_sne/calculateKLCost.m
+++ t_sne/calculateKLCost.m
@@ -2,6 +2,6 @@ function out = calculateKLCost(x,ydata,ps)
 %calculateKLCost is used by findTDistributedProjections_fmin.m to calculate
 %an optimal embedding point
 
-    d = findListDistances(x,ydata);
+    d = pdist2(x,ydata);
     d = d.^2;
     out = log(sum((1+d).^-1)) + sum(ps.*log(1+d));
